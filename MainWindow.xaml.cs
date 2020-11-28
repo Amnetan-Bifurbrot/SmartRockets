ae5e4c201d19c5e7e7d261019a832f43863bb860
Adam Wiśniewski <adamwisiwmada@gmail.com>
2020-11-28 16:26:59 +0100

    Wersja bezprzeszkodowa, z krzyżowaniem, mutacją i niespodziewanym zacięciem się programu jak robi się długą symulację.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SmartRockets {
@@ -20,20 +12,23 @@ namespace SmartRockets {
     public partial class MainWindow : Window {
         System.Windows.Threading.DispatcherTimer dispatcherTimer;
         double width = 450, height = 430, obstacleX, obstacleY;
-        int pop_size = 5; // ilosc rakiet
+        int pop_size = 10; // ilosc rakiet
         Rocket[] rockets; // docelowo bedzie duze N, wiec tablica bedzie szybsza niz lista  
- 
+        Point target = new Point(450 / 2 - 10, 90);
+        int generation = 1;
+        Random r = new Random();
+
         public MainWindow() {
             InitializeComponent();
 
             dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += dispatcherTimer_Tick;
-            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(10);
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(5);
 
             obstacleX = width / 2 - 100;
             obstacleY = height / 2 - 55;
 
-            SetObstacke(obstacleX, obstacleY, 200, 10);
+            //SetObstacle(obstacleX, obstacleY, 200, 10);
 
             rockets = new Rocket[pop_size];
         }
@@ -41,32 +36,65 @@ namespace SmartRockets {
         private void StartBtn_Click(object sender, RoutedEventArgs e) {
             dispatcherTimer.Start();
 
-            Random random = new Random();
             // generacja rakiet
             for (int i = 0; i < pop_size; i++) {
-                rockets[i] = new Rocket();  //gotowa rakieta z genami
+                //Thread.Sleep(20);
+                rockets[i] = new Rocket(r);  //gotowa rakieta z genami
                 rockets[i].pos = new Point(canvas.Width / 2 - 10, canvas.Height - 90);
 
                 canvas.Children.Add(rockets[i]);
 
                 Canvas.SetLeft(rockets[i], rockets[i].pos.X);
                 Canvas.SetTop(rockets[i], rockets[i].pos.Y);
-
+                
             }
         }
 
         private void StopBtn_Click(object sender, RoutedEventArgs e) {
             dispatcherTimer.Stop();
-            canvas.Children.Clear();
-            SetObstacke(obstacleX, obstacleY, 200, 10);
-
+            //canvas.Children.Clear();
+            //SetObstacle(obstacleX, obstacleY, 200, 10);
         }
-        private void dispatcherTimer_Tick(object sender, EventArgs e) {           
+
+        int counter = 0;
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e) {
             // rysowanie rakiet
-
+            generationLabel.Content = "Generation: " + generation;
+            if (counter < Rocket.lifetime) {
+                canvas.Children.Clear();
+                //SetObstacle(obstacleX,obstacleY,200,10);
+                for (int j = 0; j < pop_size; j++) {
+                    rockets[j].pos.X += rockets[j].genes[counter].X;
+                    rockets[j].pos.Y += rockets[j].genes[counter].Y;
+                    canvas.Children.Add(rockets[j]);
+                    Canvas.SetLeft(rockets[j], rockets[j].pos.X);
+                    Canvas.SetTop(rockets[j], rockets[j].pos.Y);
+                }
+                counter++;
+			} else {
+                counter = 0;
+                double maxFit = 0;
+                for(int i = 0; i < pop_size; i++) {
+                    rockets[i].Fitness(target);
+                    if (maxFit < rockets[i].fitness)
+                        maxFit = rockets[i].fitness;
+					Console.WriteLine("Rakieta " + i + ": " + rockets[i].fitness);
+				}
+                //dispatcherTimer.Stop();
+                Rocket[] matingPool = Rocket.CreateMatingPool(rockets);
+				Console.WriteLine(matingPool.Length);
+                for(int i = 0; i < pop_size; i++) {
+                    //Thread.Sleep(20);
+                    rockets[i] = Rocket.Procreate(matingPool, 0.001, r);
+                    rockets[i].pos = new Point(canvas.Width / 2 - 10, canvas.Height - 90);
+				}
+                generation++;
+                maxFitLabel.Content = "Max fitness: " + maxFit;
+            }
         }
 
-        private void SetObstacke(double x_coord, double y_coord, double width, double height) { // tworzy przeszkode do omijania
+        private void SetObstacle(double x_coord, double y_coord, double width, double height) { // tworzy przeszkode do omijania
             Rectangle rect;
             rect = new Rectangle();
 
--- Rocket.cs
+++ Rocket.cs
@@ -6,30 +6,34 @@ using System.Windows.Media.Imaging;
 
 namespace SmartRockets {
 	class Rocket : Image {
-		int lifetime = 200;
-		double fitness = -1;
+		public static int lifetime = 200;
+		public double fitness = -1;
 		public Point pos;
 		public Vector[] genes;
 
-
 		public Rocket() {
+			Source = new BitmapImage(new Uri("/rocket.png", UriKind.Relative));
+			Width = 20;
+			Height = 90;
+			genes = new Vector[lifetime];
+		}
+
+		public Rocket(Random r) {
 			double angle;
 			//obrazek rakiety
 			Source = new BitmapImage(new Uri("/rocket.png", UriKind.Relative));
 			Width = 20;
 			Height = 90;
-
-			Random r = new Random();
+			genes = new Vector[lifetime];
 			for (int i = 0; i < lifetime; i++) {
 				angle = r.NextDouble() * 2 * Math.PI;   //losujemy geny do chromosomu (kąty od 0 do 2pi)
-				genes[i] = new Vector(Math.Sin(angle), Math.Cos(angle));
+				genes[i] = new Vector(5*Math.Sin(angle), 5*Math.Cos(angle));
 			}
-			//jeszcze trzeba odpowiednio obrócić rakietę jakimś Transformem :) ale to pewnie już w dispatcherTimer_Tick()
 		}
 
 		//rozmnażanie
-		public void Cross(Rocket parentA, Rocket parentB) {
-			int midPoint = new Random().Next(lifetime); //losujemy punkt, w którym przedzielimy chromosom
+		public void Cross(Rocket parentA, Rocket parentB, Random r) {
+			int midPoint = r.Next(lifetime); //losujemy punkt, w którym przedzielimy chromosom
 			for (int i = 0; i < lifetime; i++) {
 				if (i < midPoint)
 					genes[i] = parentA.genes[i];
@@ -38,22 +42,29 @@ namespace SmartRockets {
 			}
 		}
 
-		public void Mutate(double chance) {
-			if (new Random().NextDouble() < chance) {
-				//tutaj będzie mutowanie
+		public void Mutate(double chance, Random r) {
+			double angle;
+			for (int i = 0; i < lifetime; i++) {
+				if (r.NextDouble() < chance) {
+					Console.WriteLine("Mutation!");
+					angle = r.NextDouble() * 2 * Math.PI;
+					genes[i] = new Vector(5 * Math.Sin(angle), 5 * Math.Cos(angle));
+				}
 			}
 		}
-		public Rocket Procreate(Rocket[] matingpool, double mutationChance) {
+
+		public static Rocket Procreate(Rocket[] matingpool, double mutationChance, Random r) {
 			Rocket child = new Rocket(), parentA, parentB;
 			int matingpoolSize = matingpool.Length;
-			//wybór rodziców z matingpool (trzeba dodać zabezpieczenie, żeby się nie losował ten sam rodzic 2 razy)
-			parentA = matingpool[new Random().Next(matingpoolSize)];
-			parentB = matingpool[new Random().Next(matingpoolSize)];
+			parentA = matingpool[r.Next(matingpoolSize)];
+			do {
+				parentB = matingpool[r.Next(matingpoolSize)];
+			} while (parentA.fitness == parentB.fitness);	//losuje rodzica dopóki nie znajdzie dwóch różnych
 
 			//krzyżowanie
-			child.Cross(parentA, parentB);
+			child.Cross(parentA, parentB, r);
 			//mutacja
-			child.Mutate(mutationChance);
+			child.Mutate(mutationChance, r);
 
 			return child;
 		}
@@ -66,9 +77,17 @@ namespace SmartRockets {
 
 		public static Rocket[] CreateMatingPool(Rocket[] population) {
 			List<Rocket> matingpool = new List<Rocket>();
-			//za Danielem Shiffmanem
-			for(int i = 0; i < population.Length; i++) {
-				double n = Math.Floor(population[i].fitness * 100);
+			double[] fitnesses = new double[population.Length];
+
+			//tutaj jest proces sortowania tablicy z rakietami
+			for (int i = 0; i < population.Length; i++)
+				fitnesses[i] = population[i].fitness;
+			Array.Sort(fitnesses,population);	//sortowanie rosnąco
+			Array.Reverse(population);	//odwrócenie tablicy, żeby była posortowana malejąco
+
+			//za Danielem Shiffmanem, ale bierzemy tylko połowę najlepszych
+			for (int i = 0; i < population.Length/2; i++) {
+				double n = Math.Floor(population[i].fitness * 1000);
 				for (int j = 0; j < n; j++)		//dodajemy rakietę o danym fitnessie odpowiednią ilość razy
 					matingpool.Add(population[i]);
 			}
